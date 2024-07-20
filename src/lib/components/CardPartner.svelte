e26486786995e5c54535f1fdb322e5ea4acfc775
Viturna <viturnapro@gmail.com>
2024-07-20 15:54:27 +0200

    avantages

---
+++ src/lib/components/CardPartner.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import "$lib/styles/styles.css";
+  export let path: string;
+  export let cardType: string;
+</script>
+
+<a href={path} class="partner-card {cardType}" aria-label="Partenaire">
+  <p class="name"><slot name="name">Nom entreprise</slot></p>
+  <div class="cadre">
+    <p class="reduc"><slot name="reduc">-00%</slot></p>
+    <p class="reduc-text"><slot name="reduc-text">sur toute la carte</slot>*</p>
+  </div>
+  <div class="card-bottom">
+    <p class="adress">
+      🏠 <slot name="adress">00 Pl. des Capucins, 33000 Bordeaux</slot>
+    </p>
+    <p class="horaire">
+      🕙 <slot name="horaire">Tous les jours : 00h00 - 00h00</slot>
+    </p>
+  </div>
+</a>
+
+<style>
+  .partner-card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    min-width: 330px;
+    width: 330px;
+    height: 330px;
+    background-image: url("/images/bg-card-partner.png");
+    border-radius: 10px;
+    text-decoration: none;
+    gap: 28px;
+  }
+
+  .partner-card.main {
+    background-color: var(--violet-secondary);
+  }
+
+  .partner-card.secondary {
+    background-color: #F3EBF4;
+  }
+
+  .name {
+    font-weight: 700;
+    font-size: 28px;
+  }
+
+  .cadre {
+    height: 100px;
+    border-radius: 10px;
+    width: 70%;
+    border: 2px solid var(--violet);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .reduc {
+    font-size: 36px;
+    font-weight: 700;
+    color: var(--violet);
+  }
+
+  .reduc-text {
+    font-size: 18px;
+    color: var(--violet);
+  }
+
+  .card-bottom {
+    padding: 0 20px;
+  }
+
+  .adress, .horaire {
+    font-size: 16px;
+  }
+</style>
--- src/routes/avantages/+page.svelte
+++ src/routes/avantages/+page.svelte
@@ -1,5 +1,104 @@
-<script>
+<script lang="ts">
+  import CardPartner from "$lib/components/CardPartner.svelte";
   import Button from "$lib/components/Button.svelte";
+
+  // Définition des partenaires
+  let partners = [
+    {
+      path: "",
+      type: "main",
+      name: "🍻 Le Grizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "secondary",
+      name: "🍻 Leizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "main",
+      name: "Grizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "secondary",
+      name: "Le FIn",
+      discount: "-10%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+  ];
+  let partnersponctuels = [
+    {
+      path: "",
+      type: "main",
+      name: "🍻 Le Grizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "secondary",
+      name: "🍻 Leizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "main",
+      name: "Grizzly",
+      discount: "-20%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+    {
+      path: "",
+      type: "secondary",
+      name: "Le FIn",
+      discount: "-10%",
+      address: "54 Pl. des Capucins, 33000 Bordeaux",
+      hours: "Tous les jours : 14h00 - 02h00",
+    },
+  ];
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    const items = document.querySelector(".partner-items") as HTMLElement | null;
+
+    // Ajuste la largeur totale des éléments
+    const adjustWidth = () => {
+      if (items) {
+        const totalWidth = Array.from(
+          document.querySelectorAll(".card-partner") as NodeListOf<HTMLElement>
+        ).reduce((width, item) => width + item.offsetWidth, 0);
+        items.style.width = `${totalWidth}px`;
+      }
+    };
+
+    // Démarre l'animation
+    const startAnimation = () => {
+      if (items) {
+        items.style.animation = "scroll 23s linear infinite";
+      }
+    };
+
+    adjustWidth();
+    startAnimation();
+
+    // Réajuste la largeur lors du redimensionnement de la fenêtre
+    window.addEventListener("resize", adjustWidth);
+  });
 </script>
 
 <section class="top-main">
@@ -14,39 +113,126 @@
       <span slot="text">Adhérer au BDE</span>
     </Button>
   </div>
-  <img src="/images/mockup-card.png" alt="Mockup des cartes adhérents" class="mockup-main" />
+  <img
+    src="/images/mockup-card.png"
+    alt="Mockup des cartes adhérents"
+    class="mockup-main"
+  />
 </section>
+
 <section class="why">
   <h2>Pourquoi adhérer au BDE ?</h2>
-  <p>Tu veux adhérer au BDE et ainsi nous aider grâce à ton achat ? On te propose 2 cartes : la carte adhérente et la carte SUPER adhérent. </p>
+  <p>
+    Tu veux adhérer au BDE et ainsi nous aider grâce à ton achat ? On te propose
+    2 cartes : la carte adhérente et la carte SUPER adhérent.
+  </p>
 </section>
+
 <section class="partner">
-  <h2>Nos partenaires</h2>
-  <p>Grâce à la carte adhérente bénéficie toute l’année de réduction chez nos partenaires.  Découvre des lieux que tu ne connais peut être pas encore !<br>
-    Parmi nos partenaires on peut compter 5 bars, 3 restaurants et 5 activités à faire seul ou à plusieurs ! Mais c’est pas tout, on a aussi des partenaires ponctuels. Dans cette catégorie t’y retrouveras des réductions pour des festivals, concert ou autre évènement temporaire. <br>
-    Pour les utiliser il te suffis de présenter ta carte au commerçant lors de ton achat.</p>
+  <div class="container-text-partner">
+    <h2>Nos partenaires</h2>
+    <p class="partner-subtitle">
+      Grâce à la carte adhérente bénéficiez toute l’année de réduction chez nos
+      partenaires. Découvre des lieux que tu ne connais peut-être pas encore !<br />
+      Parmi nos partenaires on peut compter 5 bars, 3 restaurants et 5 activités
+      à faire seul ou à plusieurs ! Mais ce n’est pas tout, on a aussi des partenaires
+      ponctuels. Dans cette catégorie, tu y retrouveras des réductions pour des festivals,
+      concerts ou autres événements temporaires. <br />
+      Pour les utiliser il te suffit de présenter ta carte au commerçant lors de
+      ton achat.
+    </p>
     <h3>Partenaires à l’année</h3>
-    <p>*sous présentation de votre carte adhérent </p>
-    <div class="flex-partner-container">
+    <p class="asterix">*sous présentation de votre carte adhérent</p>
+  </div>
 
+  <div class="partner-container">
+    <div class="partner-items">
+      {#each [...partners, ...partners] as partner (partner.name)}
+        <CardPartner
+          path="{partner.path}"
+          cardType={partner.type}
+        >
+          <span slot="name">{partner.name}</span>
+          <span slot="reduc">{partner.discount}</span>
+          <span slot="adress">{partner.address}</span>
+          <span slot="horaire">{partner.hours}</span>
+        </CardPartner>
+      {/each}
     </div>
+  </div>
+  <div class="container-text-partner">
     <h3>Partenaires Ponctuels</h3>
-    <p>*sous présentation de votre carte adhérent </p>
-    <div class="flex-partner-container">
-
+    <p class="asterix">*sous présentation de votre carte adhérent</p>
+  </div>
+  <div class="partner-container">
+    <div class="partner-items">
+      {#each [...partnersponctuels, ...partnersponctuels] as partner (partner.name)}
+        <CardPartner
+          path="{partner.path}"
+          cardType={partner.type}
+        >
+          <span slot="name">{partner.name}</span>
+          <span slot="reduc">{partner.discount}</span>
+          <span slot="adress">{partner.address}</span>
+          <span slot="horaire">{partner.hours}</span>
+        </CardPartner>
+      {/each}
     </div>
+  </div>
 </section>
+
 <section class="avantages">
   <div class="avantage-container">
-
+    <div class="avantage-number">150€</div>
+    <h2>Des économies</h2>
+    <p>
+      Economise toute l’année grâce à nos partenaires ainsi qu’au tarif réduit
+      sur la totalité de nos évènements
+    </p>
   </div>
   <div class="avantage-container">
-
+    <div class="avantage-number">15€ / 10€</div>
+    <h2>d’aide pour le bde</h2>
+    <p>
+      En prenant la carte adhérente tu aides ton BDE à mettre en place différents
+      évènements et activités.
+    </p>
   </div>
   <div class="avantage-container">
-
+    <div class="avantage-number">100%</div>
+    <h2>Achat rentabilisé</h2>
+    <p>
+      Avec tous les réductions disponibles, ton achat est rentabilisé au bout de
+      seulement 4 soirées !!
+    </p>
   </div>
 </section>
+
+<section class="convaincu">
+  <h2>Alors, On t’a <span class="violet">convaincu</span> ?</h2>
+  <p class="h2-subtitle">Commande dès maintenant ta carte adhérent</p>
+  <div class="flex-convaincu">
+    <div class="container-convaincu">
+      <div class="card">
+        <div class="card-face card-front card-sadherent"></div>
+        <div class="card-face card-back card-sadherent-back"></div>
+      </div>
+      <p class="title-convaincu">Devenir Super Adhérent</p>
+      <p class="price">15,00€</p>
+      <a href="#" class="link-convaincu">Payer en ligne</a>
+    </div>
+    <div class="container-convaincu">
+      <div class="card">
+        <div class="card-face card-front card-adherent"></div>
+        <div class="card-face card-back card-adherent-back"></div>
+      </div>
+      <p class="title-convaincu">Devenir Adhérent</p>
+      <p class="price">10,00€</p>
+      <a href="#" class="link-convaincu">Payer en ligne</a>
+    </div>
+  </div>
+</section>
+
 <style>
   .top-main {
     display: flex;
@@ -54,10 +240,10 @@
     background-color: #ae86c1;
     height: 400px;
     background-image: url("/images/lignes-fond.png");
-    position: relative; /* Pour positionner les éléments enfants correctement */
+    position: relative;
     overflow: hidden;
   }
-  .subtitle{
+  .subtitle {
     margin-bottom: 56px;
     width: 40%;
   }
@@ -66,14 +252,182 @@
     height: 780px;
     top: 0;
     right: 0;
-    z-index: 1; /* Assure que le mockup est en dessous du titre */
-
+    z-index: 1;
   }
-
   .box-title {
     position: relative;
-    z-index: 2; /* Assure que le titre est au-dessus du mockup */
+    z-index: 2;
+  }
+  .partner {
+    margin-top: 128px;
   }
 
+  .container-text-partner {
+    width: 85vw;
+    margin: 0 auto;
+  }
 
+  .partner-container {
+    overflow: hidden; /* Cache les barres de défilement */
+    position: relative;
+    width: 100%;
+    margin-top: 64px;
+  }
+
+  .partner-items {
+    gap: 64px;
+    display: flex;
+    flex-wrap: nowrap;
+    width: max-content;
+    animation: scroll 22s linear infinite;
+  }
+
+  .partner-items .reverse{
+    animation: reverse scroll 22s linear infinite;
+  }
+
+  /* Animation du défilement */
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-50%);
+    }
+  }
+  @keyframes reversescroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(50%);
+    }
+  }
+
+  .partner-container:hover .partner-items {
+    animation-play-state: paused; /* Pause l'animation au survol */
+  }
+
+  .partner-subtitle {
+    margin-top: 16px;
+    width: 75%;
+  }
+
+  .partner h3 {
+    margin-top: 86px;
+  }
+
+  .asterix {
+    font-size: 18px;
+  }
+  .avantages {
+    margin-top: 128px;
+    width: 85vw;
+    height: 550px;
+    background-color: var(--violet-2);
+    background-image: url("/images/bg-avantages.png");
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 7.5vw;
+  }
+  .avantage-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 30%;
+  }
+  .avantage-number {
+    width: fit-content;
+    border-radius: 45px;
+    font-size: 28px;
+    color: var(--violet-2);
+    font-weight: 600;
+    padding: 20px 30px;
+    background-color: var(--white);
+    margin-bottom: 56px;
+  }
+  .avantage-container p {
+    margin-top: 24px;
+  }
+  .avantage-container h2,
+  .avantage-container p {
+    color: var(--white);
+  }
+  .flex-convaincu {
+    display: flex;
+    width: 70vw;
+    margin: 128px auto;
+    justify-content: space-between;
+  }
+  .convaincu {
+    width: 85vw;
+    margin: 96px auto;
+  }
+  .h2-subtitle {
+    margin-top: 24px;
+  }
+  .convaincu h2,
+  .h2-subtitle {
+    text-align: center;
+  }
+  .container-convaincu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 28px;
+  }
+  .title-convaincu {
+    font-size: 30px;
+    font-weight: 500;
+  }
+  .price {
+    font-weight: 600;
+    font-size: 18px;
+    color: var(--violet);
+  }
+  .link-convaincu {
+    color: var(--black);
+    font-size: 18px;
+  }
+  .card {
+    width: 350px;
+    height: 225px;
+    perspective: 1000px;
+  }
+  .card-face {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    backface-visibility: hidden;
+    transition: transform 0.6s;
+  }
+  .card-front {
+    z-index: 2;
+    transform: rotateY(0deg);
+  }
+  .card-back {
+    transform: rotateY(180deg);
+  }
+  .card:hover .card-front {
+    transform: rotateY(-180deg);
+  }
+  .card:hover .card-back {
+    transform: rotateY(0deg);
+  }
+  .card-sadherent {
+    background-image: url("/images/carte-sadherent-face.png");
+  }
+  .card-sadherent-back {
+    background-image: url("/images/carte-sadherent-back.png");
+  }
+  .card-adherent {
+    background-image: url("/images/carte-adherent-face.png");
+  }
+  .card-adherent-back {
+    background-image: url("/images/carte-adherent-back.png");
+  }
 </style>
